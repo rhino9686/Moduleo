820a106791ad621a543e1ee5236e59aefcf37e4b
Robert Cecil <robcecil@umich.edu>
2021-01-17 02:40:59 -0500

    Added new iPhone App starter project

---
+++ groundControl/groundControl/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  groundControl
+//
+//  Created by Robert Cecil on 1/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ groundControl/groundControl/groundControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  groundControlApp.swift
+//  groundControl
+//
+//  Created by Robert Cecil on 1/17/21.
+//
+
+import SwiftUI
+
+@main
+struct groundControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
