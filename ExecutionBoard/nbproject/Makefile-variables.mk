b19233503dcb91ed31db24c5433f6b58b1064b0e
Robert Cecil <robcecil@umich.edu>
2021-01-21 22:15:55 -0500

    Added starter code for main execution MCU

---
+++ ExecutionBoard/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=ExecutionBoard.production.
+CND_ARTIFACT_PATH_default=dist/default/production/ExecutionBoard.production.
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=executionboard.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/executionboard.tar
