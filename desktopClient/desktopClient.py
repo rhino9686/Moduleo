895d521b004fb23704e663feeb69c909b9f61336
Robert Cecil <robertcecil@roberts-air.attlocal.net>
2021-12-04 18:32:38 -0600

    making beginnner python client

--- desktopClient/desktopClient.py
+++ desktopClient/desktopClient.py
@@ -1,1 +1,20 @@
 ## script for testing ESP8266 Server with HTTPS and Socket communication
+import websockets
+
+quitCmd = "Q"
+
+def main():
+    exiting = False
+
+
+    while (not exiting):
+
+     txt = input("Type something to test this out: ")
+     print("Is this what you just said? ", txt)
+     if txt == "Q":
+         exiting = True
+
+
+
+if __name__ == "__main__":
+    main()
