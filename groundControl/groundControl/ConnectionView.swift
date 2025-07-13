ed31173d2688dc89cd7f33f3a648a6870954333a
rhino9686 <robcecil@umich.edu>
2025-07-12 20:35:59 -0400

    significant changes to execuction board drivers, app, connectivity board,etc

---
+++ groundControl/groundControl/ConnectionView.swift
@@ -0,0 +1,18 @@
+//
+//  ConnectionView.swift
+//  groundControl
+//
+//  Created by Robert Cecil on 12/24/23.
+//
+
+import SwiftUI
+
+struct ConnectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConnectionView()
+}
